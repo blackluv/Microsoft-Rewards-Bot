1a77d341459afee1dc7907588b3a5bb95daf579a
ng-michael <ng.michael.m@gmail.com>
2018-10-17 22:00:10 -0400

    updated script with argparse, replaced api with google, optimized searches

--- ms_rewards.py
+++ ms_rewards.py
@@ -1,13 +1,17 @@
-# /usr/lib/python3.6
-# bing_rewards.py - Searches for results via pc bing browser and mobile, completes quizzes on pc bing browser
-# Attribution: https://newsapi.org - for free open source use of news api
+#! /usr/lib/python3.6
+# ms_rewards.py - Searches for results via pc bing browser and mobile, completes quizzes on pc bing browser
 
+import os
+import argparse
 import json
 import time
 import random
 import logging
-import requests
-from requests.exceptions import RequestException
+import ssl
+from _datetime import datetime, timedelta
+from urllib.error import URLError, HTTPError
+from urllib.request import urlopen
+
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException, TimeoutException, \
     ElementClickInterceptedException, ElementNotVisibleException, ElementNotInteractableException
@@ -18,8 +22,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.action_chains import ActionChains
 
-# headless? True for headless mode, false for gui mode
-headless_setting = True
 
 # URLs
 BING_SEARCH_URL = 'https://www.bing.com'
@@ -35,27 +37,109 @@ MOBILE_USER_AGENT = ('Mozilla/5.0 (Windows Phone 10.0; Android 4.2.1; WebView/3.
                      'Chrome/52.0.2743.116 Mobile Safari/537.36 Edge/15.15063')
 
 
-def get_login_dict():
+def init_logging():
+    # gets dir path of python script, not cwd, for execution on cron
+    os.chdir(os.path.dirname(os.path.realpath(__file__)))
+    os.makedirs('logs', exist_ok=True)
+    log_path = os.path.join('logs', 'ms_rewards.log')
+    logging.basicConfig(filename=log_path, level=logging.INFO,
+                        format='%(asctime)s :: %(levelname)s :: %(name)s :: %(message)s')
+
+
+def parse_args():
     """
-    Gets login usernames and passwords from json file
-    :return: Dictionary with usernames and passwords
+    Parses command line arguments for headless mode, mobile search, pc search, quiz completion
+    :return: argparse object
+    """
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('--headless', action='store_true', dest='headless_setting', default=False,
+                            help='Activates headless mode, default is off.')
+    arg_parser.add_argument('--mobile', action='store_true', dest='mobile_mode', default=False,
+                            help='Activates mobile search, default is off.')
+    arg_parser.add_argument('--pc', action='store_true', dest='pc_mode', default=False,
+                            help='Activates pc search, default is off.')
+    arg_parser.add_argument('--quiz', action='store_true', dest='quiz_mode', default=False,
+                            help='Activates pc quiz search, default is off.')
+    return arg_parser.parse_args()
+
+
+def get_dates():
+    """
+    Returns a list of dates from today to 3 days ago in year, month, day format
+    :return: list of string of dates in year, month, day format
+    """
+    dates = []
+    for i in range(0, 3):
+        date = datetime.now() - timedelta(days=i)
+        dates.append(date.strftime('%Y%m%d'))
+    return dates
+
+
+def get_search_terms():
+    dates = get_dates()
+
+    search_terms = []
+    for date in dates:
+        try:
+            url = f'https://trends.google.com/trends/api/dailytrends?hl=en-US&ed={date}&geo=US&ns=15'
+
+            response = urlopen(url, context=ssl.SSLContext(ssl.PROTOCOL_TLSv1))
+            response = json.loads(response.read()[5:])  # skips the byte junk and formatting at the beginning
+
+            for topic in response['default']['trendingSearchesDays'][0]['trendingSearches']:
+                search_terms.append(topic['title']['query'].lower())
+                for related_topic in topic['relatedQueries']:
+                    search_terms.append(related_topic['query'].lower())
+            time.sleep(random.randint(3, 10))
+        except (URLError, HTTPError):
+            logging.info('Error retrieving google trends json.')
+    logging.info(msg=f'# of search items: {len(search_terms)}\n')
+    return list(enumerate(set(search_terms)))
+
+
+def get_login_info():
+    """
+    Gets login usernames and passwords from json
+    :return: login dict
     """
     with open('ms_rewards_login_dict.json', 'r') as f:
         return json.load(f)
 
 
-def get_news_api_key():
+def browser_setup(headless_mode, user_agent):
     """
-    Gets news api key from json file
-    :return: String api key
+    Inits the firefox browser with headless setting and user agent
+    :param headless_mode: Boolean
+    :param user_agent: String
+    :return: webdriver obj
     """
-    with open('news_api_key.json', 'r') as f:
-        return json.load(f)
+    options = Options()
+    options.headless = headless_mode
+    profile = webdriver.FirefoxProfile()
+    profile.set_preference('general.useragent.override', user_agent)
+    firefox_browser_obj = webdriver.Firefox(options=options, firefox_profile=profile)
+    return firefox_browser_obj
 
 
-def init_logging():
-    logging.basicConfig(filename='ms_rewards.log', level=logging.INFO,
-                        format='%(asctime)s :: %(levelname)s :: %(message)s')
+def log_in(email_address, pass_word):
+    logging.info(msg=f'Logging in {email_address}...')
+    browser.get('https://login.live.com/')
+    time.sleep(1)
+    # wait for login form and enter email
+    wait_until_clickable(By.NAME, 'loginfmt', 30)
+    send_key_by_name('loginfmt', email_address)
+    send_key_by_name('loginfmt', Keys.RETURN)
+    logging.debug(msg='Sent Email Address.')
+    # wait for password form and enter password
+    time.sleep(1)
+    wait_until_clickable(By.NAME, 'passwd', 30)
+    send_key_by_name('passwd', pass_word)
+    logging.debug(msg='Sent Password.')
+    # wait for 'sign in' button to be clickable and sign in
+    time.sleep(1)
+    send_key_by_name('passwd', Keys.RETURN)
+    # Wait until logged in
+    wait_until_visible(By.CLASS_NAME, 'personal-info', 30)
 
 
 def find_by_id(obj_id):
@@ -105,12 +189,12 @@ def wait_until_visible(by_, selector, time_to_wait=10):
     try:
         WebDriverWait(browser, time_to_wait).until(ec.visibility_of_element_located((by_, selector)))
     except TimeoutException:
-        logging.exception(msg='Timeout Exception', exc_info=False)
+        logging.exception(msg=f'{selector} element Not Visible - Timeout Exception', exc_info=False)
     except WebDriverException:
-        logging.exception(msg='Error.')
+        logging.exception(msg=f'Webdriver error for {selector} object')
 
 
-def wait_until_clickable(by_, selector, time_to_wait=10, custom_log='TimeouException'):
+def wait_until_clickable(by_, selector, time_to_wait=10, custom_log='TimeoutException'):
     """
     Waits 5 seconds for element to be clickable
     :param by_:  BY module args to pick a selector
@@ -124,7 +208,7 @@ def wait_until_clickable(by_, selector, time_to_wait=10, custom_log='TimeouExcep
     except TimeoutException:
         logging.exception(msg=custom_log)
     except WebDriverException:
-        logging.exception(msg='Error.')
+        logging.exception(msg=f'Error., selector: {selector}')
 
 
 def send_key_by_name(name, key):
@@ -222,65 +306,32 @@ def latest_window():
     browser.switch_to.window(browser.window_handles[-1])
 
 
-def log_in(email_address, pass_word):
-    logging.info(msg=f'Logging in {email_address}...')
-    browser.get('https://login.live.com/')
-    # wait for login form and enter email
-    wait_until_clickable(By.NAME, 'loginfmt', 30)
-    send_key_by_name('loginfmt', email_address)
-    send_key_by_name('loginfmt', Keys.RETURN)
-    logging.debug(msg='Sent Email Address.')
-    # wait for password form and enter password
-    wait_until_clickable(By.NAME, 'passwd')
-    send_key_by_name('passwd', pass_word)
-    logging.debug(msg='Sent Password.')
-    # wait for 'sign in' button to be clickable and sign in
-    wait_until_clickable(By.ID, 'idSIButton9')
-    click_by_id('idSIButton9')
-    logging.debug(msg='Clicked sign-in')
-
-
-def get_news_api_search_terms(api_key):
-    news_api_url = ('https://newsapi.org/v2/everything?'
-                    'q=Cryptocurrency&'
-                    'sortBy=popularity&'
-                    'pageSize=50&'
-                    f'apiKey={api_key}')
-    try:
-        request = requests.get(news_api_url)
-        request.raise_for_status()
-        results = request.json()['articles']
-        search_terms = [results[i]['title'] for i in range(len(results))]
-        if search_terms:
-            search_terms = list(enumerate(set(search_terms), start=1))
-            logging.info(msg=f'# of search items: {len(search_terms)}\n')
-        else:
-            logging.info(msg='No search values found!')
-        return search_terms
-    except RequestException:
-        logging.exception(msg='Search API failed.')
-
-
-def search(search_terms):
+def search(search_terms, mobile_search=False):
     """
     Searches using an enumerated list of search terms, prints search item and number
     :param search_terms: enumerated list of tuples with search terms
-    :return: None
+    :param mobile_search: Boolean, True for mobile search limits, default false for pc search limits
+    :return:
     """
+    if mobile_search:
+        search_limit = 20
+    else:
+        search_limit = 30
+
     logging.info(msg="Search Start")
     if search_terms == [] or search_terms is None:
         logging.info(msg="Search Aborted. No Search Terms.")
     else:
         browser.get(BING_SEARCH_URL)
-        for num, item in search_terms:
+        for num, item in search_terms[:search_limit]:
             # clears search bar and enters in next search term
-            wait_until_clickable(By.ID, 'sb_form_q')
+            wait_until_visible(By.ID, 'sb_form_q', 10)
             clear_by_id('sb_form_q')
             send_key_by_id('sb_form_q', item)
             send_key_by_id('sb_form_q', Keys.RETURN)
             # prints search term and item, limited to 80 chars
             logging.debug(msg=f'Search #{num}: {item[:80]}')
-            time.sleep(random.randint(1, 3))
+            time.sleep(random.randint(3, 5))  # random sleep for more human-like, and let ms reward website keep up.
 
 
 def iter_dailies():
@@ -289,7 +340,6 @@ def iter_dailies():
     :return: None
     """
     browser.get(DASHBOARD_URL)
-    wait_until_clickable(By.ID, 'signinhero', 30, 'Sign in splash not detected.')
     sign_in_splash = find_by_id('signinhero')
     if sign_in_splash:
         sign_in_splash[0].click()
@@ -348,7 +398,7 @@ def explore_daily():
         # exit to main window
         main_window()
     except TimeoutException:
-        logging.exception(msg='TimeoutException.')
+        logging.exception(msg='Explore Daily Timeout Exception.')
     except (ElementNotVisibleException, ElementClickInterceptedException, ElementNotInteractableException):
         logging.exception(msg='Element not clickable or visible.')
     except WebDriverException:
@@ -419,7 +469,7 @@ def drag_and_drop_quiz():
         try:
             # find possible solution buttons
             drag_option = find_by_class('rqOption')
-            # find any answers marked correct with green color
+            # find any answers marked correct with correctAnswer tag
             right_answers = find_by_class('correctAnswer')
             # remove right answers from possible choices
             if right_answers:
@@ -477,22 +527,6 @@ def get_point_total():
     logging.info(msg=f'Mobile points = {mobile_points}')
 
 
-def browser_setup(headless_mode, user_agent):
-    """
-    Inits the firefox browser with headless setting and user agent
-    :param headless_mode: Boolean
-    :param user_agent: String
-    :return: webdriver obj
-    """
-
-    options = Options()
-    options.headless = headless_mode
-    profile = webdriver.FirefoxProfile()
-    profile.set_preference('general.useragent.override', user_agent)
-    firefox_browser_obj = webdriver.Firefox(options=options, firefox_profile=profile)
-    return firefox_browser_obj
-
-
 if __name__ == '__main__':
     try:
         # start logging
@@ -500,36 +534,54 @@ if __name__ == '__main__':
         logging.info(msg='---------------------------------')
         logging.info(msg='---------------------------------')
 
-        # get api key and get search terms
-        news_api_key = get_news_api_key()
-        search_list = get_news_api_search_terms(news_api_key)
+        # argparse
+        parser = parse_args()
+        logging.info(msg='args parsed.')
 
         # get login dict
-        login_dict = get_login_dict()
+        login_dict = get_login_info()
+        logging.info(msg='logins retrieved.')
+
+        # get search terms
+        search_list = []
+        if parser.mobile_mode or parser.pc_mode:
+            search_list = get_search_terms()
+
         # iter through accounts, search, and complete quizzes
         for email, password in login_dict.items():
-            # MOBILE MODE
-            logging.info(msg='************MOBILE***************')
-            # set up headless browser and mobile user agent
-            browser = browser_setup(headless_setting, MOBILE_USER_AGENT)
-            log_in(email, password)
-            # mobile search
-            search(search_list)
-            browser.quit()
 
-            # PC MODE
-            logging.info(msg='************PC***************')
-            # set up edge headless browser and edge pc user agent
-            browser = browser_setup(headless_setting, PC_USER_AGENT)
-            log_in(email, password)
-            # pc edge search
-            search(search_list)
-            # complete quizzes
-            iter_dailies()
-            # get point totals
-            get_point_total()
-            browser.quit()
+            if parser.mobile_mode:
+                # MOBILE MODE
+                logging.info(msg='************MOBILE***************')
+                # set up headless browser and mobile user agent
+                browser = browser_setup(parser.headless_setting, MOBILE_USER_AGENT)
+                try:
+                    log_in(email, password)
+                    # mobile search
+                    search(search_list, mobile_search=True)
+                    browser.quit()
+                except KeyboardInterrupt:
+                    browser.quit()
+
+            if parser.pc_mode or parser.quiz_mode:
+                # PC MODE
+                logging.info(msg='************PC***************')
+                # set up edge headless browser and edge pc user agent
+                browser = browser_setup(parser.headless_setting, PC_USER_AGENT)
+                try:
+                    log_in(email, password)
+                    if parser.pc_mode:
+                        # pc edge search
+                        search(search_list)
+                    if parser.quiz_mode:
+                        # complete quizzes
+                        iter_dailies()
+                    # get point totals
+                    get_point_total()
+                    browser.quit()
+                except KeyboardInterrupt:
+                    browser.quit()
 
             logging.info(msg='\n\n')
     except WebDriverException:
-        logging.exception(msg='Process error')
+        logging.exception(msg='Webdriver failure.')
